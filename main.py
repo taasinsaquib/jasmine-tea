c8e4ff8ae53006bc3463d862839c4793af7bb783
Luke Rivers <luke.rivers9@outlook.com>
2020-11-14 11:17:57 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import markovify
 import requests
 from bs4 import BeautifulSoup
+
+def getsoup_episodes(url):
+    
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    episodes = soup.find_all("a", {"class": "category-page__member-link"})
+    transcript_titles = [episode.string for episode in episodes]
+    
+    return transcript_titles
+
+transcript_titles = getsoup_episodes('https://avatar.fandom.com/wiki/Category:Avatar:_The_Last_Airbender_episode_transcripts?oldid=1052211')
 
 def getsoup(url):
     
@@ -10,8 +23,8 @@ def getsoup(url):
     
     return tables
 
-x = getsoup('https://avatar.fandom.com/wiki/Transcript:The_Last_Airbender')
-
+x = getsoup('https://avatar.fandom.com/wiki/Transcript:The_Last_Airbender') 
+    
 def extract(table):
     
     quotes = []
@@ -23,5 +36,8 @@ def extract(table):
             
     print(quotes)
     
-    
-a = extract(x[1])
+
+for transcript in transcript_titles:
+    x = getsoup('https://avatar.fandom.com/wiki/'+transcript)
+    for i in range(len(x)):
+        a = extract(x[i])
