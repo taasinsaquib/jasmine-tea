5493992625ee5a725f0f70a73e13e3637c247b30
ELEANOR PAE <eleanorpae@g.ucla.edu>
2020-11-14 00:40:43 -0800

    create dictionary for character names and quotes

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
+
 import requests
 from bs4 import BeautifulSoup
 
+
 def getsoup(url):
-    
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    
+
     tables = soup.find_all('table', {"class": "wikitable"})
-    
+
     return tables
+
 
 x = getsoup('https://avatar.fandom.com/wiki/Transcript:The_Last_Airbender')
 
+
 def extract(table):
-    
-    quotes = []
+    quotes = {}
     lines = table.find_all('tr')
     for line in lines:
         childTag = line.find('th')
         if childTag:
-            quotes.append(line)
-            
+            name = line.find_all('th')[0].string
+            speech = line.find_all('td')[0].string
+            if name in quotes:
+                quotes[name].append(speech)
+            else:
+                quotes[name] = [speech]
+
     print(quotes)
-    
-    
+    # print(len(quotes.keys()))
+
+
 a = extract(x[1])
