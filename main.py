3f785cb9d1313b04d36e545bd0056e001792d628
Luke Rivers <luke.rivers9@outlook.com>
2020-11-14 00:00:45 -0800

    Update main.py
    Step 1: Iterating through webpage tables.
    
    Extract lines spoken by characters.

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get('https://avatar.fandom.com/wiki/Transcript:The_Deserter')
-soup = BeautifulSoup(page.content, 'html.parser')
+def getsoup(url):
+    
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    tables = soup.find_all('table', {"class": "wikitable"})
+    
+    return tables
 
-tables = soup.find_all('table')
-print(tables)
-print(tables[0].prettify())
+x = getsoup('https://avatar.fandom.com/wiki/Transcript:The_Last_Airbender')
+
+def extract(table):
+    
+    quotes = []
+    lines = table.find_all('tr')
+    for line in lines:
+        childTag = line.find('th')
+        if childTag:
+            quotes.append(line)
+            
+    print(quotes)
+    
+    
+a = extract(x[1])
